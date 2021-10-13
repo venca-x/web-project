96f59b51e968554d5a107461d102a6c926bba795
vEnCa-X <vencax@gmail.com>
2021-10-13 22:29:04 +0200

    Upgrade to stable Nette 3.1

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App;
 
-use Nette\Configurator;
+use Nette\Bootstrap\Configurator;
 
 
 class Bootstrap
@@ -12,22 +12,24 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$configurator = new Configurator;
+		$appDir = dirname(__DIR__);
+
 
 		$secret = 'fgdfgfghghhfdsdfghfgx'; //set random chars
 		$configurator->setDebugMode([$secret . '@23.75.345.200']); // enable for your remote IP
 		//setcookie('nette-debug', $secret, strtotime('1 years'));
-
-		$configurator->enableTracy(__DIR__ . '/../log');
+		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../temp');
+		$configurator->setTempDirectory($appDir . '/../temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
-		$configurator->addConfig(__DIR__ . '/config/common.neon');
-		$configurator->addConfig(__DIR__ . '/config/local.neon');
+		$configurator->addConfig($appDir . '/config/common.neon');
+		$configurator->addConfig($appDir . '/config/services.neon');
+		$configurator->addConfig($appDir . '/config/local.neon');
 
 		return $configurator;
 	}
