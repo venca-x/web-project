e9a4a16add8d5a38768022df5d5b852235dd7594
vEnCa-X <vencax@gmail.com>
2026-04-26 10:26:38 +0200

    aktualiyace gulp sass - potlaceni warningu, breaking chnges

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,28 @@ var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify-es').default;
 var cleanCSS = require('gulp-clean-css');
 var shell = require('gulp-shell');
+var { finished } = require('stream/promises');
+
+/**
+ * Volby pro Dart Sass, aby build neplácal desítky varování z kódu, na který teď neovlivníme.
+ * - quietDeps: ztiší varování ze souborů z node_modules (Bootstrap, Font Awesome).
+ * - silenceDeprecations: @import, globální built-in funkce a staré color.* (z knihoven);
+ *   postupně @use/moduly, až půjde refaktor,
+ *   až budeme moci zvednout verze nebo refaktorovat vlastní entry SCSS.
+ */
+var sassDepSilence = {
+    quietDeps: true,
+    silenceDeprecations: ['import', 'global-builtin', 'color-functions'],
+};
+var bootstrapSassPath = {
+    loadPaths: ['./node_modules/bootstrap/scss',
+        './node_modules/bootstrap/scss/forms',
+        './node_modules/bootstrap/scss/helpers',
+        './node_modules/bootstrap/scss/mixins',
+        './node_modules/bootstrap/scss/utilities',
+        './node_modules/bootstrap/scss/vendor'],
+};
+var faSassPath = { loadPaths: ['./node_modules/@fortawesome/fontawesome-free/scss'] };
 
 gulp.task('shell_composer_self_update', shell.task('composer self-update'));
 gulp.task('shell_composer_update', shell.task('composer update'));
@@ -17,49 +39,33 @@ gulp.task('shell_netteCodingStandard', shell.task('php ..\\nette-coding-standard
 gulp.task('shell_netteCodingStandardFIX', shell.task('php ..\\nette-coding-standard\\ecs check app tests --preset php81 --fix'));
 gulp.task('shell_phpstan', shell.task('c:\\www\\phpstan-nette\\vendor\\bin\\phpstan.bat analyse app tests --level=6 --memory-limit=4000M'));
 
-gulp.task('sass', done => {
-    gulp.src(['src/scss-admin/fontawesome.scss'])
-        .pipe(sass({includePaths: ['./node_modules/@fortawesome/fontawesome-free/scss']}).on('error', sass.logError))
+gulp.task('sass', async () => {
+    // main.scss a admin importují ../css/tmp/*.css; musí existovat až po FA výstupech
+    const fa1 = gulp.src(['src/scss-admin/fontawesome.scss'])
+        .pipe(sass({ ...sassDepSilence, ...faSassPath }).on('error', sass.logError))
         .pipe(concat('fontawesome.css'))
         .pipe(gulp.dest('src/css/tmp'));
-
-    gulp.src(['src/scss-admin/fontawesome-brands.scss'])
-        .pipe(sass({includePaths: ['./node_modules/@fortawesome/fontawesome-free/scss']}).on('error', sass.logError))
+    const fa2 = gulp.src(['src/scss-admin/fontawesome-brands.scss'])
+        .pipe(sass({ ...sassDepSilence, ...faSassPath }).on('error', sass.logError))
         .pipe(concat('fontawesome-brands.css'))
         .pipe(gulp.dest('src/css/tmp'));
-
-    gulp.src(['src/scss-admin/fontawesome-solid.scss'])
-        .pipe(sass({includePaths: ['./node_modules/@fortawesome/fontawesome-free/scss']}).on('error', sass.logError))
+    const fa3 = gulp.src(['src/scss-admin/fontawesome-solid.scss'])
+        .pipe(sass({ ...sassDepSilence, ...faSassPath }).on('error', sass.logError))
         .pipe(concat('fontawesome-solid.css'))
         .pipe(gulp.dest('src/css/tmp'));
+    await Promise.all([finished(fa1), finished(fa2), finished(fa3)]);
 
-    gulp.src(['src/scss/main.scss'])
-        .pipe(sass({
-            includePaths: ['./node_modules/bootstrap/scss',
-                './node_modules/bootstrap/scss/forms',
-                './node_modules/bootstrap/scss/helpers',
-                './node_modules/bootstrap/scss/mixins',
-                './node_modules/bootstrap/scss/utilities',
-                './node_modules/bootstrap/scss/vendor']
-        }).on('error', sass.logError))
+    const m1 = gulp.src(['src/scss/main.scss'])
+        .pipe(sass({ ...sassDepSilence, ...bootstrapSassPath }).on('error', sass.logError))
         .pipe(cleanCSS())
         .pipe(concat('main.min.css'))
         .pipe(gulp.dest('www/css'));
-
-    gulp.src(['src/scss-admin/main.scss'])
-        .pipe(sass({
-            includePaths: ['./node_modules/bootstrap/scss',
-                './node_modules/bootstrap/scss/forms',
-                './node_modules/bootstrap/scss/helpers',
-                './node_modules/bootstrap/scss/mixins',
-                './node_modules/bootstrap/scss/utilities',
-                './node_modules/bootstrap/scss/vendor']
-        }).on('error', sass.logError))
+    const m2 = gulp.src(['src/scss-admin/main.scss'])
+        .pipe(sass({ ...sassDepSilence, ...bootstrapSassPath }).on('error', sass.logError))
         .pipe(cleanCSS())
         .pipe(concat('admin.min.css'))
         .pipe(gulp.dest('www/css'));
-
-    done();
+    await Promise.all([finished(m1), finished(m2)]);
 });
 
 gulp.task('jsMain', done => {
